1a82a9dbad4cf6e2530844e95b931f2ca578ca57
Liza Iutina <iutinaelizaveta@yandex.ru>
2022-10-26 21:56:08 +0400

    MESTO

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ let profileEditButton = document.querySelector(".profile__button-edit");
 let popupCloseButton = document.querySelector(".pop-up__exit");
 let popup = document.querySelector(".pop-up");
 
+let profileName = document.querySelector(".profile__name");
+let profileProfession = document.querySelector(".profile__profession");
+let inputName = document.querySelector(".form__input_info_name");
+let inputProfession = document.querySelector(".form__input_info_profession");
+
+function addValueInput() {
+  inputName.value = profileName.textContent;
+  inputProfession.value = profileProfession.textContent;
+}
 
 function openPopup() {
   popup.classList.add("popup_opened");
+  addValueInput();
 }
 
 function closePopup() {
