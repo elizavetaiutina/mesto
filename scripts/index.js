e4f3fbe3b96231cd915c036c51654f106bb86864
Liza Iutina <iutinaelizaveta@yandex.ru>
2022-11-08 23:53:44 +0400

    feature: кнока делит удаляет первую карту

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,16 @@ let exitPopupEditProfile = popupEditProfile.querySelector(".pop-up__exit");
 let popupAddCard = document.querySelector(".pop-up_type_add-card");
 let exitPopupAddCard = popupAddCard.querySelector(".pop-up__exit");
 
+//Удаления карточки
+ const handleDeleteCard = (event) =>{
+  event.target.closest(".card").remove()
+ }
+
+ let card = document.querySelector(".card");
+let cardDeleteButton = card.querySelector(".card__button-delete");
+cardDeleteButton.addEventListener("click", handleDeleteCard)
+
+
 // Находим поля профиля
 let profileName = document.querySelector(".profile__name");
 let profileProfession = document.querySelector(".profile__profession");
@@ -32,11 +42,17 @@ profileEditButton.addEventListener("click", () => {
   nameInput.value = profileName.textContent;
   jobInput.value = profileProfession.textContent;
 });
-exitPopupEditProfile.addEventListener("click",() => {closePopup(popupEditProfile)});
+exitPopupEditProfile.addEventListener("click", () => {
+  closePopup(popupEditProfile);
+});
 
 // Обработчики событии (кликов) для открытия и закрытия поп-апа "добавления карточек"
-cardAddButton.addEventListener("click", () => {openPopup(popupAddCard)});
-exitPopupAddCard.addEventListener("click", () => {closePopup(popupAddCard)});
+cardAddButton.addEventListener("click", () => {
+  openPopup(popupAddCard);
+});
+exitPopupAddCard.addEventListener("click", () => {
+  closePopup(popupAddCard);
+});
 
 // Обработчик «отправки» формы поп-апа "редактирования профиля", хотя пока она никуда отправляться не будет
 function formSubmitHandler(form) {
@@ -50,3 +66,31 @@ function formSubmitHandler(form) {
 
 // Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
 formElement.addEventListener("submit", formSubmitHandler);
+
+//Массив 6 карточек, загрузку на страницу которых добавляет JavaScript
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
