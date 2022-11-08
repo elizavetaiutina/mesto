862a4da0dd74fc0077349db666c7375cfdd4d1e3
Liza Iutina <iutinaelizaveta@yandex.ru>
2022-11-08 23:39:47 +0400

    feature: кнока делит, попап для карт, Js открытия и закрытия попапов

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,10 @@
 // Находим кнопки открытия и закрытия поп-апа
 let profileEditButton = document.querySelector(".profile__button-edit");
-let popupCloseButton = document.querySelector(".pop-up__exit");
-let popup = document.querySelector(".pop-up");
+let cardAddButton = document.querySelector(".profile__button-add");
+let popupEditProfile = document.querySelector(".pop-up_type_edit-profile");
+let exitPopupEditProfile = popupEditProfile.querySelector(".pop-up__exit");
+let popupAddCard = document.querySelector(".pop-up_type_add-card");
+let exitPopupAddCard = popupAddCard.querySelector(".pop-up__exit");
 
 // Находим поля профиля
 let profileName = document.querySelector(".profile__name");
@@ -14,22 +17,28 @@ let jobInput = document.querySelector(".form__input_info_profession");
 let saveButton = document.querySelector(".form__button-save");
 
 // Функция открытия поп-апа
-function openPopup() {
+function openPopup(popup) {
   popup.classList.add("pop-up_opened");
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileProfession.textContent;
 }
 
 // Функция закрытия поп-апа
-function closePopup() {
+function closePopup(popup) {
   popup.classList.remove("pop-up_opened");
 }
 
-// Обработчики событии (кликов) для открытия и закрытия поп-апа
-profileEditButton.addEventListener("click", openPopup);
-popupCloseButton.addEventListener("click", closePopup);
+// Обработчики событии (кликов) для открытия и закрытия поп-апа "редактирования профиля"
+profileEditButton.addEventListener("click", () => {
+  openPopup(popupEditProfile);
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileProfession.textContent;
+});
+exitPopupEditProfile.addEventListener("click",() => {closePopup(popupEditProfile)});
 
-// Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
+// Обработчики событии (кликов) для открытия и закрытия поп-апа "добавления карточек"
+cardAddButton.addEventListener("click", () => {openPopup(popupAddCard)});
+exitPopupAddCard.addEventListener("click", () => {closePopup(popupAddCard)});
+
+// Обработчик «отправки» формы поп-апа "редактирования профиля", хотя пока она никуда отправляться не будет
 function formSubmitHandler(form) {
   form.preventDefault();
 
