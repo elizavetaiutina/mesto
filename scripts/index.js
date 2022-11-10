3d588962dd295803dc6a9b6d18a7763504670d8e
Liza Iutina <iutinaelizaveta@yandex.ru>
2022-11-10 13:31:05 +0400

    feature: добавление карточек через js и template

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,69 @@
+//Массив 6 карточек, загрузку на страницу которых добавляет JavaScript
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+
+const cardContainer = document.querySelector(".gallery__card-list"); //контейнер для карт
+//const formAddCard = document.querySelector(".form"); //форма создания новой карточки
+//const inputNameCard = formAddCard.querySelector(".form__input_info_name-place"); //поле ввода названия карточки
+//const inputUrlCard = formAddCard.querySelector(".form__input_info_url-place"); //поле ввода ссылки
+
+//Шаблон template для карт
+const cardTemplate = document.querySelector(".card-template").content; //.querySelector(".card");
+
+// Создание карточки (template)
+
+function generateCard(item) {
+  const newCard = cardTemplate.cloneNode(true);
+
+  const nameCard = newCard.querySelector(".card__name");
+  nameCard.textContent = item.name;
+
+  const urlCard = newCard.querySelector(".card__image");
+  urlCard.src = item.link;
+  urlCard.alt = item.name;
+
+  return newCard;
+}
+
+// Добавление карточки
+function renderCard(item) {
+  cardContainer.prepend(generateCard(item));
+}
+
+// Проходим по массиву и создаем для каждого элемента добавление карточки
+initialCards.forEach(function (item) {
+  renderCard(item);
+});
+
+
+
+
+
+
 // Находим кнопки открытия и закрытия поп-апа
 let profileEditButton = document.querySelector(".profile__button-edit");
 let cardAddButton = document.querySelector(".profile__button-add");
@@ -54,7 +120,7 @@ exitPopupAddCard.addEventListener("click", () => {
 });
 
 // Обработчик «отправки» формы поп-апа "редактирования профиля", хотя пока она никуда отправляться не будет
-const formSubmitHandler = (form) => {
+const formEditProfileSubmitHandler = (form) => {
   form.preventDefault();
 
   profileName.textContent = nameInput.value;
@@ -64,32 +130,4 @@ const formSubmitHandler = (form) => {
 };
 
 // Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
-formElement.addEventListener("submit", formSubmitHandler);
-
-//Массив 6 карточек, загрузку на страницу которых добавляет JavaScript
-const initialCards = [
-  {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-  {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
-];
+formElement.addEventListener("submit", formEditProfileSubmitHandler);
