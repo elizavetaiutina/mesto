600053319d5b3efdfccdd3a46e6aa65190df54a0
Liza Iutina <iutinaelizaveta@yandex.ru>
2022-11-29 00:21:21 +0400

    fixes

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,87 +1,93 @@
-
-//Функция, которая добавляет класс с ошибкой — показывает элемент ошибки;
-const showInputError = (formElement, inputElement, errorMessage) => {
-  // Находим элемент ошибки внутри самой функции
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add("form__input_type_error");
-  // Заменим содержимое span с ошибкой на переданный параметр
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add("form__input-error_active");
+// объект параметров для валидации
+const objectValidation = {
+  formSelector: ".form",
+  inputSelector: ".form__input",
+  submitButtonSelector: ".form__button-save",
+  inactiveButtonClass: "form__button-save_disabled",
+  inputErrorClass: "form__input_type_error-bottom-red",
+  errorClass: "form__span-error_active",
 };
 
-//Функция, которая удаляет класс с ошибкой
-const hideInputError = (formElement, inputElement) => {
-  // Находим элемент ошибки
+//Функции, первая отвечают за показ ошибки, если поле не проходит валидацию и вторая наоборот, ее скроет
+const showInputError = (
+  formElement,
+  inputElement,
+  errorMessage,
+  objectValidation
+) => {
+  // Находим спан поле для ошибки
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove("form__input_type_error");
-  errorElement.classList.remove("form__input-error_active");
+  inputElement.classList.add(objectValidation.inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(objectValidation.errorClass);
+};
+
+const hideInputError = (formElement, inputElement, objectValidation) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove(objectValidation.inputErrorClass);
+  errorElement.classList.remove(objectValidation.errorClass);
   errorElement.textContent = "";
 };
 
-// Функция, которая проверяет валидность поля, внутри вызывает showInputError или hideInputError
-const isValid = (formElement, inputElement) => {
+// Функция, которая проверяет валидность поля
+const isValid = (formElement, inputElement, objectValidation) => {
   if (!inputElement.validity.valid) {
-    // showInputError теперь получает параметром форму, в которой находится проверяемое поле, и само это поле
-    showInputError(formElement, inputElement, inputElement.validationMessage); // Если поле не проходит валидацию, покажем ошибку
+    showInputError(
+      formElement,
+      inputElement,
+      inputElement.validationMessage,
+      objectValidation
+    );
   } else {
-    // hideInputError теперь получает параметром форму, в которой находится проверяемое поле, и само это поле
-    hideInputError(formElement, inputElement); // Если проходит, скроем ошибку
+    hideInputError(formElement, inputElement, objectValidation);
   }
 };
 
-// Принимает параметром элемент формы и добавляет её инпутам нужные обработчики
-const setEventListeners = (formElement) => {
-  const inputList = Array.from(formElement.querySelectorAll('.form__input'));
-  const buttonForm = formElement.querySelector('.form__button-save');
-  toggleButtonState(inputList, buttonForm);
+const setEventListeners = (formElement, objectValidation) => {
+  const inputList = Array.from(
+    formElement.querySelectorAll(objectValidation.inputSelector)
+  );
+  const buttonForm = formElement.querySelector(
+    objectValidation.submitButtonSelector
+  );
+  toggleButtonState(inputList, buttonForm, objectValidation);
 
   inputList.forEach((inputElement) => {
-    inputElement.addEventListener('input', () => {
-      isValid(formElement, inputElement);
+    inputElement.addEventListener("input", () => {
+      isValid(formElement, inputElement, objectValidation);
 
-      // Вызовем toggleButtonState и передадим ей массив полей и кнопку
-      toggleButtonState(inputList, buttonForm);
+      toggleButtonState(inputList, buttonForm, objectValidation);
     });
   });
 };
 
-// массив полей перебираем методом some
+//Кнопка
 const hasInvalidInput = (inputList) => {
   return inputList.some((inputElement) => {
     // Если поле не валидно, колбэк вернёт true и обход массива прекратится
     return !inputElement.validity.valid;
-  })
+  });
 };
 
-// Функция принимает массив полей ввода и элемент кнопки, состояние которой нужно менять
-const toggleButtonState = (inputList, buttonForm) => {
+const toggleButtonState = (inputList, buttonForm, objectValidation) => {
   // Если есть хотя бы один невалидный инпут делаем кнопку неактивной иначе активной
   if (hasInvalidInput(inputList)) {
-    buttonForm.classList.add('form__button-save_disabled');
-    buttonForm.setAttribute('disabled', true);
+    buttonForm.classList.add(objectValidation.inactiveButtonClass);
+    buttonForm.setAttribute("disabled", true);
   } else {
-    buttonForm.classList.remove('form__button-save_disabled');
-    buttonForm.removeAttribute('disabled', true);
+    buttonForm.classList.remove(objectValidation.inactiveButtonClass);
+    buttonForm.removeAttribute("disabled", true);
   }
 };
 
-// Находим и перебераем все формы черед метод массива, вызывая для каждой формы функцию setEventListeners
-const enableValidation = () => {
-  const formList = Array.from(document.querySelectorAll('.form'));
+const enableValidation = (objectValidation) => {
+  const formList = Array.from(
+    document.querySelectorAll(objectValidation.formSelector)
+  );
   formList.forEach((formElement) => {
-    setEventListeners(formElement);
-    });
+    setEventListeners(formElement, objectValidation);
+  });
 };
 
-// объект параметров для валидации
-const objectValidation = {
-  formSelector: '.form',
-  inputSelector: '.form__input',
-  submitButtonSelector: '.form__button-save',
-  inactiveButtonClass: '.form__buttonsave_disabled',
-  inputErrorClass: 'form__input-error',
-  errorClass: 'form__input-error_active'
-}
-
-// функция enableValidation , которая включает валидацию, принимает на вход объект параметров
+// Принимает на вход объект параметров
 enableValidation(objectValidation);
